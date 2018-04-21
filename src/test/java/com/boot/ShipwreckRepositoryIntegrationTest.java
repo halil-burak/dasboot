631f01648a1e77450583adfd03dc6ce96994ca2f
hburak <hburak3534@gmail.com>
2018-04-21 15:06:54 +0300

    Web Integration Test Added

--- src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
+++ src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
@@ -6,16 +6,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootApplication
+@SpringBootTest(classes = App.class)
 public class ShipwreckRepositoryIntegrationTest {
 
     @Autowired
@@ -25,5 +26,13 @@ public class ShipwreckRepositoryIntegrationTest {
     public void testFindAll() {
         List<Shipwreck> wrecks = shipwreckRepository.findAll();
         assertThat(wrecks.size(), is(greaterThanOrEqualTo(0)));
+    }
+
+    @Test
+    public void testFindOne() {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1l);
+        Shipwreck shipwreck = shipwreckRepository.getOne(1l);
+        assertEquals(shipwreck.getId().longValue(), sw.getId().longValue());
     }
 }
